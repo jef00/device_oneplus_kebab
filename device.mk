b37d7be4051d507ad6acf5e1df3458106a8951c3
Broly <jpegrande@gmail.com>
2022-11-15 13:31:54 -0500

    PixelExperience: bringup

--- device.mk
+++ device.mk
@@ -32,7 +32,7 @@ $(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
